eb9a7b7dd03a1371bffb7848bf2a5ba57eee0733
Mikkel <mikkel@hacklite.net>
2017-12-19 10:53:47 +0100

    Add WithTranslateProps to withTranslate HOC

--- src/hoc/withTranslate.tsx
+++ src/hoc/withTranslate.tsx
@@ -1,6 +1,11 @@
 import React, { createElement, Component, ComponentType } from 'react'
 import * as PropTypes from 'prop-types'
 
+export interface WithTranslateProps {
+  t(text: string, data?: Object, context?: string): string
+  tn(n: number, text: string, textPlural: string, data?: Object, context?: string | null): string
+}
+
 export const withTranslate = (WrappedComponent: ComponentType) => {
   return class WithTranslate extends Component {
     static contextTypes = {
