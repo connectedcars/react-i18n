f82fe0e0aabe04907b98c2d06b287b21a41f6adc
Mikkel <mikkel@hacklite.net>
2018-02-12 12:08:02 +0100

    Update example to use store state

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -6,14 +6,18 @@ import './index.css'
 
 import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
-import I18n, { i18nReducer } from '@connectedcars/react-i18n'
+import I18n, { i18nReducer, I18nState } from '@connectedcars/react-i18n'
+
+export interface StoreState {
+  i18n: I18nState
+}
 
 const translations = require('./translations.json')
 
-const appReducer = combineReducers({
+const appReducer = combineReducers<StoreState>({
   i18n: i18nReducer
 })
-const store = createStore(appReducer)
+const store = createStore<StoreState>(appReducer)
 
 ReactDOM.render(
   <Provider store={store}>
