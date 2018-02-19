6fdb02edbbbaf9245ca0aaee30705ee8e41b51ff
Mikkel <mikkel@hacklite.net>
2018-02-19 11:58:33 +0100

    Add missing types to withTranslate and make a types file

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as PropTypes from 'prop-types'
 import { getTranslation, replace, Translations } from '../translate'
+import { TranslateFunc, TranslatePluralFunc } from '../types';
 
 export interface ProviderProps {
   lang?: string
@@ -39,14 +40,14 @@ export class Provider extends Component<ProviderProps> {
     }
   }
 
-  translate = (translations: Translations, lang?: string) => {
+  translate = (translations: Translations, lang?: string): TranslateFunc => {
     return (text: string, data?: Object, context?: string) => {
       const str = getTranslation(translations, lang, 0, text, null, context)
       return replace(str, 0, data)
     }
   }
 
-  ntranslate = (translations: Translations, lang?: string) => {
+  ntranslate = (translations: Translations, lang?: string): TranslatePluralFuncFunc => {
     return (n: number, text: string, textPlural: string, data?: Object, context?: string | null) => {
       const str = getTranslation(translations, lang, n, text, textPlural, context)
       return replace(str, n, data)
