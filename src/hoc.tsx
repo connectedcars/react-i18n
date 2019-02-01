846ad19c3f01728725c0a4f9bb726f0521cc58b5
Mikkel <mikkel@hacklite.net>
2019-02-01 13:28:15 +0100

    Add type for with translate

--- src/hoc.tsx
+++ src/hoc.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { I18nContext, I18nContextProps } from './context'
 
+export interface WithTranslateProps extends I18nContextProps {}
+
 function withTranslate<T = {}>(
-  BaseComponent: React.ComponentType<T & I18nContextProps>
+  BaseComponent: React.ComponentType<T & WithTranslateProps>
 ): React.ComponentType<T> {
   return class WithTranslate extends React.Component<T> {
     static displayName = `withTranslate(${BaseComponent.displayName})`
